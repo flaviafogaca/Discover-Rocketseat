098bbd6a41e4d4dab4258511203741a883e1e9de
flaviafogaca <flaviamariafogaca@gmail.com>
2022-05-30 15:26:00 -0400

    tipos de dados

---
+++ Fundamentar/guia-estelar-js/script.js
@@ -0,0 +1,102 @@
+// Tipos de dados
+
+// String
+
+/*
+console.log('Olá, mundo!')
+
+console.log("Olá, 'mundo!'")
+
+console.log(`Olá "Mundo '!'"`)
+
+console.log(`Olá
+Mundo`
+)
+
+console.log(`Olá ${1 + 1}`)
+
+console.log(1 + 1)
+*/
+ 
+// Number
+
+/*
+    33 // inteiros
+    12.5 // reais - float
+    NaN // Not a Number
+    Infinity // infinito 
+
+console.log(33)
+
+console.log(12.5 / "asdf")
+
+console.log(12.5 === Infinity)
+*/
+
+// Boolean
+
+/*
+   true // verdadeiro
+    false // falso
+
+console.log(2 == 2)
+*/
+
+// Undefined vs null
+
+/*
+    * Undefined 
+        * indefinido
+        
+    * Null
+        * nulo
+        * objeto que não possui nada dentro
+        * diferente de indefinido
+
+
+console.log(undefined)
+
+console.log(null)
+
+console.log(null === undefined)
+*/
+
+// Object
+
+/* 
+    * Objeto
+    * Propredade ou Atributos
+    * Funcionalidades ou Métodos
+
+    { propriedade: "valor" }
+
+console.log({
+    name: "Flávia",
+    idade: 26,
+    andar: function() {
+        console.log('andar')
+    }
+})
+*/
+
+// Array (Vetores)
+
+/*
+    * Uma lista
+    * Agrupamento de dados
+    
+    ["Flávia, 26"]
+*/
+
+console.log(["Flávia, 36"])
+
+console.log([
+    "Flávia",
+    26
+])
+
+console.log([
+    "gato",
+    "cachorro",
+    "pássaro"
+])
