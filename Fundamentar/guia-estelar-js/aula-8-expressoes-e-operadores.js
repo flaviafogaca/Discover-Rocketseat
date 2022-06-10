a90844956d30c4b63852c6d66a3d905ac5b7e00a
flaviafogaca <flaviamariafogaca@gmail.com>
2022-06-10 15:36:18 -0400

    conhecendo expressões e operadores

---
+++ Fundamentar/guia-estelar-js/aula-8-expressoes-e-operadores.js
@@ -0,0 +1,60 @@
+/* Expressões e Operadores
+
+-Expressions
+-Operators
+    Binary
+    Unary
+    Ternary
+
+let number = 1
+
+console.log(true ? 'Alô' : 'nada')
+*/
+
+/* 
+    new
+*/
+
+/*
+let name = new String('Flávia')
+name.surName = "Fogaça"
+let age = new Number(23)
+console.log(name.surName, age)
+*/
+
+/*
+    Operadores unários (Mais comuns: typeof e delete)
+*/
+
+/*
+const person = {
+    name: 'Flávia',
+    age:26
+}
+console.log(person)
+
+delete person.age
+
+console.log(person)
+*/
+
+/*
+Operadores Aritiméticos
+
+console.log(3 * 2) // Multiplicação
+
+console.log(10 / 2) // Divisão
+
+console.log(10 + 2) //Soma
+
+console.log(5 - 3) //Subtração
+
+console.log(10 % 2) // Resto da divisão
+
+let number = 10
+console.log(++number) // Incrementa (Pode ser usado antes ou depois de "number", mas se usar depois, não vai mostrar direto. Tem outras formas incrementar fora do console)
+
+console.log(--number) // Decrementa (Similar ao incremento)
+
+console.log(10 ** 2) // Exponecial
+*/
