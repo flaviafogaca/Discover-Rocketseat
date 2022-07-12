992bea7361e3bdffd3e47b4da6bf76fb04494c56
flaviafogaca <flaviamariafogaca@gmail.com>
2022-07-12 14:36:23 -0400

    Manipulando conteúdos

---
+++ Fundamentar/10-Pilotando-com-a-DOM/script.js
@@ -0,0 +1,42 @@
+// ---> MANIPULANDO CONTEÚDO
+
+// getElementById()
+
+/* console.log(document.getElementById('blog-title'))
+
+const element = document.getElementsByTagName('h1')
+console.log(element)
+*/
+
+// textContent
+
+/*
+const element = document. querySelector('h1')
+
+element.textContent += " Olá Devs!"
+
+console.log(element.textContent)
+*/
+
+// innerText
+
+/*
+const element = document.querySelector('h1')
+
+element.innerText = "Olá Devs"
+*/
+
+// innerHTML
+
+/*
+const element = document.querySelector('h1')
+
+element.innerHTML = "Olá Devs! <small>!!!</small>"
+*/
+
+// Manipulando conteúdo: value
+
+const element = document.querySelector('input')
+
+element.value = "Valor"
+
