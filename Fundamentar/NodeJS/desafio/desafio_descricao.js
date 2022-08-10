7d107baa14224ec532386153f2473fddab794210
flaviafogaca <flaviamariafogaca@gmail.com>
2022-08-10 15:28:57 -0400

    estudando NodeJS + desafio

---
+++ Fundamentar/NodeJS/desafio/desafio_descricao.js
@@ -0,0 +1,6 @@
+/* Crie 2 arquivos JavaScript;
+* O primeiro ira exportar uma função chamada getFlag() que receberá um argumento do tipo String;
+* Essa função deverá buscar dentro do array process.argv a flag deseja, que é a sting e retornar o valor da flag;
+* O segundo irá importar a função e pasar a flag desejada;
+* Iremos rodar no terminal o segundo arquivo passando as flags --name e --greeting;
+*/
