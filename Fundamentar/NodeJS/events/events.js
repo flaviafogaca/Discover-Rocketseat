774734f17a87605b88797a612c32e02d2334de81
flaviafogaca <flaviamariafogaca@gmail.com>
2022-10-11 16:54:22 -0400

    event once

--- Fundamentar/NodeJS/events/events.js
+++ Fundamentar/NodeJS/events/events.js
@@ -2,11 +2,13 @@ const { EventEmitter } = require('events')
 
 const ev = new EventEmitter()
 
-ev.on('saySomething', (message) => {
+ev.once('saySomething', (message) => {
     console.log('Eu ouvi você: ', message)
 })
 
 ev.emit('saySomething', "Flávia")
+ev.emit('saySomething', "Danielle")
+ev.emit('saySomething', "Maria")
 
 
 
